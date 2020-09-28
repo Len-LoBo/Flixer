55842d1a2bbb73c68e9570c21e41b45cfd77bd3d
Len LoBo <llobiondo2@gmail.com>
2020-09-28 11:22:53 -0700

    toolbar added

--- app/src/main/java/com/lenlobo/flixer/activities/MovieListActivity.java
+++ app/src/main/java/com/lenlobo/flixer/activities/MovieListActivity.java
@@ -1,6 +1,7 @@
 package com.lenlobo.flixer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -32,9 +33,12 @@ public class MovieListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         RecyclerView rvMovies = findViewById(R.id.rvMovies);
         movies = new ArrayList<>();
-
 
         //create adapter
         final MovieAdapter movieAdapter = new MovieAdapter(this, movies);
